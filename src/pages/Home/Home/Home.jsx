9c11a4de94ec8ca1daa733cef26a151cb3fc9de2
MUKHTER <mohammadmukhter@gmail.com>
2023-06-19 12:28:39 +0600

    multivendor ecommerce client section initialize and essential defendency installed

---
+++ src/pages/Home/Home/Home.jsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <div className="max-w-7xl mx-auto">
+      <h3>This is home page</h3>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/pages/Shared/NavBar/NavBar.jsx
@@ -0,0 +1,12 @@
+import SearchBar from "./SearchBar";
+
+const NavBar = () => {
+  return (
+    <div>
+      <SearchBar></SearchBar>
+      <h3>This is navBar</h3>
+    </div>
+  );
+};
+
+export default NavBar;
