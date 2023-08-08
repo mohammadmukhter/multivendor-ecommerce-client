8c0ab24ccf9ad72583f8480ce8d33094ef88d462
mac <mac@MUKHTERS-MACBOOK.local>
2023-08-09 02:40:22 +0600

    cart functionality continued

--- utils/addCartUtil.js
+++ utils/addCartUtil.js
@@ -1,5 +1,11 @@
-const addCartUtil = (productId, userEmail= "") => {
+import axios from "axios";
+
+const addCartUtil = async(productId, userEmail= "") => {
+    const addCartData = await axios.post("http://localhost:3000/carts", {productId, userEmail});
+    console.log(addCartData);
+
     return {productId, userEmail}
+
 };
 
 export default addCartUtil;
